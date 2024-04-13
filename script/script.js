84eace7e0916dd867c50b363ef44a29300cf844c
Rodrigo Andrade <144704436+soadse@users.noreply.github.com>
2024-04-12 23:21:58 -0300

    projeto signos

---
+++ script/script.js
@@ -0,0 +1,462 @@
+var telaLogin = document.querySelector('#containerLogin')
+var telaEspera = window.document.querySelector('#containerEspera')
+var telaSignos = document.querySelector('#containerSignos')
+var erro = document.querySelector('#erro')
+
+
+
+
+// CAMPO DOS NOMES
+var nome1 = window.document.querySelector('#nome1')
+var nome2 = window.document.querySelector('#nome2')
+var nome3 = window.document.querySelector('#nome3')
+var nome4 = window.document.querySelector('#nome4')
+var nome5 = window.document.querySelector('#nome5')
+var nome6 = window.document.querySelector('#nome6')
+var nome7 = window.document.querySelector('#nome7')
+var nome8 = window.document.querySelector('#nome8')
+var nome9 = window.document.querySelector('#nome9')
+var nome10 = window.document.querySelector('#nome10')
+var nome11= window.document.querySelector('#nome11')
+var nome12= window.document.querySelector('#nome12')
+// CAMPO DAS DATAS 
+var data1 = window.document.querySelector('#data1')
+var data2 = window.document.querySelector('#data2')
+var data3 = window.document.querySelector('#data3')
+var data4 = window.document.querySelector('#data4')
+var data5 = window.document.querySelector('#data5')
+var data6 = window.document.querySelector('#data6')
+var data7 = window.document.querySelector('#data7')
+var data8 = window.document.querySelector('#data8')
+var data9 = window.document.querySelector('#data9')
+var data10 = window.document.querySelector('#data10')
+var data11= window.document.querySelector('#data11')
+var data12= window.document.querySelector('#data12')
+
+
+
+// CAMPO DOS SIGNOS
+var aries = document.querySelector('#aries')
+var touro = document.querySelector('#touro')
+var gemeos = document.querySelector('#gemeos')
+var cancer = document.querySelector('#cancer')
+var leao = document.querySelector('#leao')
+var virgem = document.querySelector('#virgem')
+var libra = document.querySelector('#libra')
+var escorpiao = document.querySelector('#escorpiao')
+var sagitario = document.querySelector('#sagitario')
+var capricornio = document.querySelector('#capricornio')
+var aquario = document.querySelector('#aquario')
+var peixes = document.querySelector('#peixes')
+
+
+
+//  CAMPO DOS ELEMENTOS
+
+//aries
+var aries1 = window.document.querySelector('#aries1')
+var aries2 = window.document.querySelector('#aries2')
+var aries3 = window.document.querySelector('#aries3')
+function mostra1(){
+    aries1.style.display = 'flex'
+    aries2.style.display = 'none'
+    aries3.style.display = 'none'
+}
+function mostra2(){
+    aries1.style.display = 'none'
+    aries2.style.display = 'flex'
+    aries3.style.display = 'none'
+}
+function mostra3(){
+    aries1.style.display = 'none'
+    aries2.style.display = 'none'
+    aries3.style.display = 'flex'  
+}
+//touro
+var touro1 = window.document.querySelector('#touro1')
+var touro2 = window.document.querySelector('#touro2')
+var touro3 = window.document.querySelector('#touro3')
+function mostra4(){
+    touro1.style.display = 'flex'
+    touro2.style.display = 'none'
+    touro3.style.display = 'none'
+}
+function mostra5(){
+    touro1.style.display = 'none'
+    touro2.style.display = 'flex'
+    touro3.style.display = 'none'
+}
+function mostra6(){
+    touro1.style.display = 'none'
+    touro2.style.display = 'none'
+    touro3.style.display = 'flex'  
+}
+//gêmeos
+var gemeos1 = window.document.querySelector('#gemeos1')
+var gemeos2 = window.document.querySelector('#gemeos2')
+var gemeos3 = window.document.querySelector('#gemeos3')
+function mostra7(){
+    gemeos1.style.display = 'flex'
+    gemeos2.style.display = 'none'
+    gemeos3.style.display = 'none'
+}
+function mostra8(){
+    gemeos1.style.display = 'none'
+    gemeos2.style.display = 'flex'
+    gemeos3.style.display = 'none'
+}
+function mostra9(){
+    gemeos1.style.display = 'none'
+    gemeos2.style.display = 'none'
+    gemeos3.style.display = 'flex'  
+}
+//câncer
+var cancer1 = window.document.querySelector('#cancer1')
+var cancer2 = window.document.querySelector('#cancer2')
+var cancer3 = window.document.querySelector('#cancer3')
+function mostra10(){
+    cancer1.style.display = 'flex'
+    cancer2.style.display = 'none'
+    cancer3.style.display = 'none'
+}
+function mostra11(){
+    cancer1.style.display = 'none'
+    cancer2.style.display = 'flex'
+    cancer3.style.display = 'none'
+}
+function mostra12(){
+    cancer1.style.display = 'none'
+    cancer2.style.display = 'none'
+    cancer3.style.display = 'flex'  
+}
+//leão
+var leao1 = window.document.querySelector('#leao1')
+var leao2 = window.document.querySelector('#leao2')
+var leao3 = window.document.querySelector('#leao3')
+function mostra13(){
+    leao1.style.display = 'flex'
+    leao2.style.display = 'none'
+    leao3.style.display = 'none'
+}
+function mostra14(){
+    leao1.style.display = 'none'
+    leao2.style.display = 'flex'
+    leao3.style.display = 'none'
+}
+function mostra15(){
+    leao1.style.display = 'none'
+    leao2.style.display = 'none'
+    leao3.style.display = 'flex'  
+}
+//virgem
+var virgem1 = window.document.querySelector('#virgem1')
+var virgem2 = window.document.querySelector('#virgem2')
+var virgem3 = window.document.querySelector('#virgem3')
+function mostra16(){
+    virgem1.style.display = 'flex'
+    virgem2.style.display = 'none'
+    virgem3.style.display = 'none'
+}
+function mostra17(){
+    virgem1.style.display = 'none'
+    virgem2.style.display = 'flex'
+    virgem3.style.display = 'none'
+}
+function mostra18(){
+    virgem1.style.display = 'none'
+    virgem2.style.display = 'none'
+    virgem3.style.display = 'flex'  
+}
+//libra
+var libra1 = window.document.querySelector('#libra1')
+var libra2 = window.document.querySelector('#libra2')
+var libra3 = window.document.querySelector('#libra3')
+function mostra19(){
+    libra1.style.display = 'flex'
+    libra2.style.display = 'none'
+    libra3.style.display = 'none'
+}
+function mostra20(){
+    libra1.style.display = 'none'
+    libra2.style.display = 'flex'
+    libra3.style.display = 'none'
+}
+function mostra21(){
+    libra1.style.display = 'none'
+    libra2.style.display = 'none'
+    libra3.style.display = 'flex'  
+}
+//escorpião
+var escorpiao1 = window.document.querySelector('#escorpiao1')
+var escorpiao2 = window.document.querySelector('#escorpiao2')
+var escorpiao3 = window.document.querySelector('#escorpiao3')
+function mostra22(){
+    escorpiao1.style.display = 'flex'
+    escorpiao2.style.display = 'none'
+    escorpiao3.style.display = 'none'
+}
+function mostra23(){
+    escorpiao1.style.display = 'none'
+    escorpiao2.style.display = 'flex'
+    escorpiao3.style.display = 'none'
+}
+function mostra24(){
+    escorpiao1.style.display = 'none'
+    escorpiao2.style.display = 'none'
+    escorpiao3.style.display = 'flex'  
+}
+//sagitário
+var sagitario1 = window.document.querySelector('#sagitario1')
+var sagitario2 = window.document.querySelector('#sagitario2')
+var sagitario3 = window.document.querySelector('#sagitario3')
+function mostra25(){
+    sagitario1.style.display = 'flex'
+    sagitario2.style.display = 'none'
+    sagitario3.style.display = 'none'
+}
+function mostra26(){
+    sagitario1.style.display = 'none'
+    sagitario2.style.display = 'flex'
+    sagitario3.style.display = 'none'
+}
+function mostra27(){
+    sagitario1.style.display = 'none'
+    sagitario2.style.display = 'none'
+    sagitario3.style.display = 'flex'  
+}
+//capricórnio
+var capricornio1 = window.document.querySelector('#capricornio1')
+var capricornio2 = window.document.querySelector('#capricornio2')
+var capricornio3 = window.document.querySelector('#capricornio3')
+function mostra28(){
+    capricornio1.style.display = 'flex'
+    capricornio2.style.display = 'none'
+    capricornio3.style.display = 'none'
+}
+function mostra29(){
+    capricornio1.style.display = 'none'
+    capricornio2.style.display = 'flex'
+    capricornio3.style.display = 'none'
+}
+function mostra30(){
+    capricornio1.style.display = 'none'
+    capricornio2.style.display = 'none'
+    capricornio3.style.display = 'flex'  
+}
+//aquário
+var aquario1 = window.document.querySelector('#aquario1')
+var aquario2 = window.document.querySelector('#aquario2')
+var aquario3 = window.document.querySelector('#aquario3')
+function mostra31(){
+    aquario1.style.display = 'flex'
+    aquario2.style.display = 'none'
+    aquario3.style.display = 'none'
+}
+function mostra32(){
+    aquario1.style.display = 'none'
+    aquario2.style.display = 'flex'
+    aquario3.style.display = 'none'
+}
+function mostra33(){
+    aquario1.style.display = 'none'
+    aquario2.style.display = 'none'
+    aquario3.style.display = 'flex'  
+}
+//peixes
+var peixes1 = window.document.querySelector('#peixes1')
+var peixes2 = window.document.querySelector('#peixes2')
+var peixes3 = window.document.querySelector('#peixes3')
+function mostra34(){
+    peixes1.style.display = 'flex'
+    peixes2.style.display = 'none'
+    peixes3.style.display = 'none'
+}
+function mostra35(){
+    peixes1.style.display = 'none'
+    peixes2.style.display = 'flex'
+    peixes3.style.display = 'none'
+}
+function mostra36(){
+    peixes1.style.display = 'none'
+    peixes2.style.display = 'none'
+    peixes3.style.display = 'flex'  
+}
+
+
+
+
+
+
+
+
+
+
+function pega(){
+    var nome = window.document.querySelector('#nome').value
+    var d = window.document.querySelector('#dia')
+    var dia = Number(d.value)
+    var m = window.document.querySelector('#mes')
+    var mes = Number(m.value)  
+    // CAMPO NOME
+    nome.innerText = ''
+    if(nome == ''){
+        erro.innerHTML = 'Campo <strong>nome</strong> vazio'
+    } else if(nome.length < 2) {
+        erro.innerText = 'Digite um nome válido'
+    } else if(nome == Number(nome)){
+        erro.innerHTML = 'Somente números não é permitido no campo <strong>nome</strong>'
+    }
+    // CAMPO DIA
+    else if(dia == ''){
+        erro.innerHTML = 'Campo <strong>dia</strong> vazio'
+    } else if(dia < 0 || dia > 31){
+        erro.innerText = 'Digite um dia entre 01 e 31'
+    }
+    // CAMPO MÊS
+    else if(mes == ''){
+        erro.innerHTML = 'Campo <strong>mês</strong> vazio'
+    } else if(mes < 0 || mes > 12){
+        erro.innerText = 'Digite um mês entre 01 e 12'
+    } else {
+        telaLogin.style.display = 'none'
+        telaEspera.style.display = 'block'
+        telaSignos.style.display = 'block'
+    }
+
+
+    
+    // CAMPO DOS SIGNOS
+
+    // SIGNO DE ARIES
+    if(dia >= 21 && mes == 3){
+        aries.style.display = 'block'
+        nome1.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data1.innerHTML = `${dia}/${mes}`
+    } else if(dia < 21 && mes == 4){
+        aries.style.display = 'block'
+        nome1.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data1.innerHTML = `${dia}/${mes}`
+    }
+
+    // SIGNO DE TOURO
+    if(dia >= 21 && mes == 4){
+        touro.style.display = 'block'
+        nome2.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data2.innerHTML = `${dia}/${mes}`
+    } else if(dia < 21 && mes == 5){
+        touro.style.display = 'block'
+        nome2.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data2.innerHTML = `${dia}/${mes}`
+    }
+
+    // SIGNO DE GÊMEOS
+    if(dia >= 21 && mes == 5){
+        gemeos.style.display = 'block'
+        nome3.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data3.innerHTML = `${dia}/${mes}`
+    } else if(dia < 21 && mes == 6){
+        gemeos.style.display = 'block'
+        nome3.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data3.innerHTML = `${dia}/${mes}`
+    }
+
+    // SIGNO DE CÂNCER  
+    if(dia >= 21 && mes == 6){
+        cancer.style.display = 'block'
+        nome4.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data4.innerHTML = `${dia}/${mes}`
+    } else if(dia < 23 && mes == 7){
+        cancer.style.display = 'block'
+        nome4.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data4.innerHTML = `${dia}/${mes}`
+    }
+
+    // SIGNO DE LEÃO
+    if(dia >= 23 && mes == 7){
+        leao.style.display = 'block'
+        nome5.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data5.innerHTML = `${dia}/${mes}`
+    } else if(dia < 23 && mes== 8){
+        leao.style.display = 'block'
+        nome5.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data5.innerHTML = `${dia}/${mes}`
+    }
+    
+    // SIGNO DE VIRGEM
+    if(dia >= 23 && mes == 8){
+        virgem.style.display = 'block'
+        nome6.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data6.innerHTML = `${dia}/${mes}`
+    } else if(dia < 23 && mes == 9){
+        virgem.style.display = 'block'
+        nome6.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data6.innerHTML = `${dia}/${mes}`
+    }
+
+    // SIGNO DE LIBRA
+    if(dia >= 23 && mes == 9){
+        libra.style.display = 'block'
+        nome7.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data7.innerHTML = `${dia}/${mes}`
+    } else if(dia < 23 && mes == 10){
+        libra.style.display = 'block'
+        nome7.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data7.innerHTML = `${dia}/${mes}`
+    }
+
+    // SIGNO DE ESCORPIÃO
+    if(dia >= 23 && mes == 10){
+        escorpiao.style.display = 'block'
+        nome8.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data8.innerHTML = `${dia}/${mes}`
+    } else if(dia < 22 && mes == 11){
+        escorpiao.style.display = 'block'
+        nome8.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data8.innerHTML = `${dia}/${mes}`
+    }
+
+    // SIGNO DE SAGITÁRIO
+    if(dia >= 22 && mes == 11){
+        sagitario.style.display = 'block'
+        nome9.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data9.innerHTML = `${dia}/${mes}`
+    } else if(dia < 22 && mes == 12){
+        sagitario.style.display = 'block'
+        nome9.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data9.innerHTML = `${dia}/${mes}`
+    }
+
+    // SIGNO DE CAPRICÓRNIO
+    if(dia >= 22 && mes == 12){
+        capricornio.style.display = 'block'
+        nome10.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data10.innerHTML = `${dia}/${mes}`
+    } else if(dia < 21 && mes == 1){
+        capricornio.style.display = 'block'
+        nome10.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data10.innerHTML = `${dia}/${mes}`
+    }
+
+    // SIGNO DE AQUÁRIO
+    if(dia >= 21 && mes == 1){
+        aquario.style.display = 'block'
+        nome11.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data11.innerHTML = `${dia}/${mes}`
+    } else if(dia < 19 && mes == 2){
+        aquario.style.display = 'block'
+        nome11.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data11.innerHTML = `${dia}/${mes}`
+    }
+
+    // SIGNO DE PEIXES
+    if(dia >= 19 && mes == 2){
+        peixes.style.display = 'block'
+        nome12.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data12.innerHTML = `${dia}/${mes}`
+    } else if(dia < 21 && mes == 3){
+        peixes.style.display = 'block'
+        nome12.innerHTML = `Bem Vindo<br>${nome}`.toLocaleUpperCase()
+        data12.innerHTML = `${dia}/${mes}`
+    }
+
+}
